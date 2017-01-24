e522e2ab4ee60fc063218d67dc08ea6d239bfc55
ybarra <joe@thinkbean.com>
2017-01-24 13:23:25 -0500

    inital commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+
+import Main from './components/Main';
+
+class App extends Component {
+  render() {
+    const store = createStore(
+      reducers,
+      applyMiddleware(ReduxThunk),
+    );
+    return (
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+  }
+}
+
+export default App;
